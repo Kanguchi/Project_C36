89d34c641694c789c07baa5e5c6888f080b79197
KanoKit <kano.kitaguchi@gmail.com>
2020-10-07 17:43:26 -0400

    added getFedTime funciton

--- food.js
+++ food.js
@@ -1,30 +1,37 @@
 class food{
     constructor(){
-        var foodStock, lastFed;
+        this.foodStock = 0;
+        this.lastFed;
         this.image = loadImage("Milk.png");
     }
-    getFoodStock(){
-
+    
+    updateFoodStock(foodStock){
+        this.foodStock = foodStock;
     }
-    updateFoodStock(){
-
+    getFoodStock(){
+        return this.foodStock = this.foodStock-1;
     }
     deductFood(){
-
+        if(this.foodStock > 0){
+            this.foodStock = this.foodStock - 1;
+        }
+    }
+    getFedTime(lastFed){
+        this.lastFed = lastFed;
     }
     display(){
-        var x = 80, y = 100;
+        var x = 35, y = 60;
 
         imageMode(CENTER);
-        image(this.image, 720, 220, 70, 70);
+        image(this.image, 500, 750, 70, 70);
 
         if (this.foodStock != 0){
           for(var i = 0; i<this.foodStock; i++){
-              if(i%10 == 0){
-                  x=80;
+              if(i%4 == 0){
+                  x=35;
                   y=y+50;
               }
-              this.image(this.image, x, y, 50, 50);
+              image(this.image, x, y, 50, 50);
               x=x+30;
           }
         }
